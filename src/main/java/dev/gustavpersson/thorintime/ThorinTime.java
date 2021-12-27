bf075856ec06ac89043ed47308fc0340082ea99d
ThorinEk <mailtogustav7@gmail.com>
2021-12-27 22:31:12 +0100

    Initial commit

---
+++ src/main/java/dev/gustavpersson/thorintime/ThorinTime.java
@@ -0,0 +1,17 @@
+package dev.gustavpersson.thorintime;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ThorinTime extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
